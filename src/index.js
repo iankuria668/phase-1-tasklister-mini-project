6bf6f049b41c85e747c2814a300c8c60fd3c0b5f
iankuria668 <iankuria668@gmail.com>
2024-04-07 14:11:26 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const form = document.getElementById("create-task-form");
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const input = document.getElementById("new-task-description");
+    const description = input.value;
+    input.value = "";
+    
+    const item = document.createElement("li");
+    item.innerText = description;
+    document.getElementById("tasks").appendChild(item);
+
+    const list = document.getElementById("tasks");
+    list.appendChild(item);
+    });
+  });
+
